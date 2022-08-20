01726d04fe4379314ba1aea82165a760ebb68c6b
Maksym Lytovchenko <lmb1988@ukr.net>
2022-08-20 15:53:44 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,46 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
-console.log('hello');
+const listForRender = document.querySelector(".gallery");
+const imageMarkup = createGalleryMarkup(galleryItems);
+listForRender.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `  <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+listForRender.addEventListener("click", onListForRenderClick);
+
+function onListForRenderClick(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const bigImgSrc = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${bigImgSrc}" width="800" height="600" >
+`);
+  instance.show(window.addEventListener("keydown", escHandler));
+
+  function escHandler(e) {
+    if (e.code === "Escape") {
+      instance.close(window.removeEventListener("keydown", escHandler));
+    }
+  }
+}
