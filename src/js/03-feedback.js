077e0aef639442094daccef2a7f1f563ed111720
Maksym Lytovchenko <lmb1988@ukr.net>
2022-09-21 16:12:11 +0300

    update task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,12 +11,7 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormsSubmit);
 refs.form.addEventListener('input', throttle(onFormsInput, 500));
-// refs.message.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.email.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.form.addEventListener('input', event => {
-//   formData[event.target.name] = event.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// });
+
 function onFormsInput(event) {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
@@ -33,23 +28,12 @@ function onFormsSubmit(e) {
 populateTextarea();
 function populateTextarea() {
   const saveMessage = localStorage.getItem(STORAGE_KEY);
-  const objV = Object.values(JSON.parse(saveMessage));
-  const objK = Object.keys(JSON.parse(saveMessage));
-  if (!saveMessage) {
-    return null;
+  const obj = JSON.parse(saveMessage);
+
+  if (obj?.message) {
+    refs.message.value = obj.message;
   }
-  if (objK.includes('message')) {
-    refs.message.value = objV;
-    console.log(objK);
-  }
-  if (objK.includes('email')) {
-    refs.email.value = objV;
-    console.log(objK);
+  if (obj?.email) {
+    refs.email.value = obj.email;
   }
 }
-
-// const parsObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
-// const userInput = localStorage.getItem(STORAGE_KEY);
-// const parseData = JSON.parse(userInput);
-// console.log(parsObject);
-// console.log(userInput);
